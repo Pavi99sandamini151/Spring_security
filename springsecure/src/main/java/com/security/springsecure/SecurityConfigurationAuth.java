d8c567e7f0ce581d6a7d88e972006358c0573ce1
Pavi99 <pavithra@smbapps.com.au>
2023-01-01 11:46:01 +0530

    specific user added

--- springsecure/src/main/java/com/security/springsecure/SecurityConfigurationAuth.java
+++
@@ -1,7 +0,0 @@
-package com.security.springsecure;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
-public interface SecurityConfigurationAuth {
-    void configure(AuthenticationManagerBuilder auth) throws Exception;
-}
--- springsecure/src/main/java/com/security/springsecure/WebSecurityConfigurerAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.security.springsecure;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
-public abstract class WebSecurityConfigurerAdapter {
-
-    protected abstract void configure(AuthenticationManagerBuilder auth) throws Exception;
-}
